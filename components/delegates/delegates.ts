285e9fde71c4eeaf3b594d2a4711f6b4e95cc5b7
mnairobi <nicholuskiriinya7@gmail.com>
2026-02-05 09:02:18 +0300

    Delegates

--- components/delegates/delegates.ts
+++ components/delegates/delegates.ts
@@ -74,6 +74,171 @@ const delegateItems = [
         school: "Pure, Applied and Health Sciences",
         name: "Wambui",
     },
+      {
+        image: "2brandonHOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Brandon",
+    },
+      {
+        image: "2brendaHOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Brenda",
+    },
+      {
+        image: "2DicksonHOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Dickson",
+    },
+      {
+        image: "2ikramARTS.jpeg",
+        school: "Education",
+        name: "Ikram",
+    },
+      {
+        image: "2juliusAPP.jpeg",
+        school: "Pure, Applied and Health Sciences",
+        name: "Julius",
+    },
+    {
+        image: "2santanaHOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Santana",
+    },
+    {
+        image: "2sudiHOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Sudi",
+    },
+    {
+        image: "2vallaryHOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Vallary",
+    },
+    {
+        image: "Abbie2BCOM.jpeg",
+        school: "Business and Economics",
+        name: "Abigael",
+    },
+    {
+        image: "abigael2SS.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Abigael",
+    },
+    {
+        image: "brenda2PUR.jpeg",
+        school: "Pure, Applied and Health Sciences",
+        name: "Brenda",
+    },
+    {
+        image: "casper2EDU.jpeg",
+        school: "Education",
+        name: "Casper",
+    },
+    {
+        image: "cecilia2HRM.jpeg",
+        school: "Business and Economics",
+        name: "Cecilia",
+    },
+    {
+        image: "cynthia2PUR.jpeg",
+        school: "Pure, Applied and Health Sciences",
+        name: "Cynthia",
+    },
+    {
+        image: "elosy2SS.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Elosy",
+    },
+    {
+        image: "eric2HRM.jpeg",
+        school: "Business and Economics",
+        name: "Eric",
+    },
+    {
+        image: "Faith2HUM.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Faith",
+    },
+    {
+        image: "george2EDU.jpeg",
+        school: "Education",
+        name: "George",
+    },
+    {
+        image: "james2EDU.jpeg",
+        school: "Education",
+        name: "James",
+    },
+    {
+        image: "james2SS.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "James",
+    },
+     {
+        image: "joyce2BCOM.jpeg",
+        school: "Business and Economics",
+        name: "Joyce",
+    },
+     {
+        image: "lucy2AGED.jpeg",
+        school: "Agriculture and Environmental Sciences",
+        name: "Lucy",
+    },
+     {
+        image: "melyne2HOS.jpeg",
+        school: "Hospitality and Tourism Management",
+        name: "Melyne",
+    },
+     {
+        image: "michael2BCOM.jpeg",
+        school: "Business and Economics",
+        name: "Michael",
+    },
+     {
+        image: "mildred2BCOM.jpeg",
+        school: "Business and Economics",
+        name: "Mildred",
+    },
+     {
+        image: "perez2HRM.jpeg",
+        school: "Business and Economics",
+        name: "Perez",
+    },
+     {
+        image: "ronny2HUM.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Ronny",
+    },
+     {
+        image: "ruth2EDU.jpeg",
+        school: "Education",
+        name: "Ruth",
+    },
+     {
+        image: "sheila2HUM.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Sheila",
+    },
+     {
+        image: "valentine2HUM.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Valentine",
+    },
+     {
+        image: "victor2IT.jpeg",
+        school: "Computing and IT",
+        name: "Victor",
+    },
+     {
+        image: "villary2EDU.jpeg",
+        school: "Education",
+        name: "Villary",
+    },
+     {
+        image: "vincent2SS.jpeg",
+        school: "Humanities and Social Sciences",
+        name: "Vincent",
+    },
 ]
 
 export  { delegateItems }
